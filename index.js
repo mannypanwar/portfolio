30b308e9a0a47a3aa23826597e82ca96733a9ed3
Mandeep Singh Panwar <fit.panwar@gmail.com>
2020-07-05 12:37:33 +0530

    form fixes

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ gsap.from(".navbar-brand, .nav-list", {
   opacity: 0,
   scale: 0.3,
   ease: "back",
+  delay: 0.5,
 });
+
+document
+  .querySelector(".submit-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+  });
