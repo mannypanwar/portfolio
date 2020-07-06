3dab6e4800b9520e809c5f49976b3aa398269cab
Mandeep Singh Panwar <fit.panwar@gmail.com>
2020-07-06 19:13:12 +0530

    removing on window load function

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 gsap.registerPlugin(ScrollTrigger);
 
-window.addEventListener("load", function () {
-  init();
+gsap.from("body", {
+  backgroundColor: "#fff",
+  opacity: 0,
+  duration: 0.5,
+  ease: "none",
 });
 
-function init() {
-  gsap.from("body", {
-    backgroundColor: "#fff",
-    opacity: 0,
-    duration: 0.5,
-    ease: "none",
-  });
-
-  gsap.from(".navbar-brand, .nav-list", {
-    duration: 3,
-    opacity: 0,
-    scale: 0.3,
-    ease: "back",
-    delay: 0.5,
-  });
-}
+gsap.from(".navbar-brand, .nav-list", {
+  duration: 3,
+  opacity: 0,
+  scale: 0.3,
+  ease: "back",
+  delay: 0.5,
+});
