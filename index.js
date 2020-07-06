206422c31205086238fa1dbace811b7913ad9611
Mandeep Singh Panwar <fit.panwar@gmail.com>
2020-07-06 18:52:49 +0530

    animation experiment with gsap

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+gsap.registerPlugin(ScrollTrigger);
+
 gsap.from(".navbar-brand, .nav-list", {
   duration: 1.5,
   opacity: 0,
   scale: 0.3,
   ease: "back",
   delay: 0.5,
+});
+
+gsap.to(".hero", {
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".main",
+    start: "top top",
+    // end: "bottom center",
+    scrub: true,
+  },
+});
+
+gsap.to([".p-Text", ".p-projects"], {
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".portfolio",
+    start: "top top",
+    // end: "bottom center",
+    scrub: true,
+  },
+});
+gsap.to([".blog-Text", ".blog-list"], {
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".blog",
+    start: "top top",
+    // end: "bottom center",
+    scrub: true,
+  },
 });
